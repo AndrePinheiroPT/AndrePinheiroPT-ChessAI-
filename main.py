5c5ba17ca13c6f5c755a26badd1a53f57aa699cf
André Pinheiro <andreftmarcelo@gmail.com>
2022-06-30 19:19:22 +0100

    Fix bugs part 1

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ def board():
     for i in range(0, 8):
         for j in range(0, 8):
             pygame.draw.rect(screen, white if (i + j) % 2 == 0 else brown, pygame.Rect(i*SQUARE_LENTH, j*SQUARE_LENTH, SQUARE_LENTH, SQUARE_LENTH))
+    for i in range(0, 8):
+        for j in range(0, 8):
             if board_pieces_surfaces[j][i] == 0:
                 continue
             screen.blit(board_pieces_surfaces[j][i], board_pieces_surfaces_position[j][i])
@@ -54,17 +56,38 @@ def piece():
             board_pieces_surfaces_position[j][i][0] = i*SQUARE_LENTH
             board_pieces_surfaces_position[j][i][1] = j*SQUARE_LENTH
 
-hold_piece = False
+piece_selected_code = None
 
-def grab_piece(mouse_pos):
-    global hold_piece
+def grab_piece():
+    global piece_selected_code, mouse_state
     for i in range(0, 8):
         for j in range(0, 8):
-            if mouse_pressed and j*SQUARE_LENTH <= mouse_pos[1] < SQUARE_LENTH*(j + 1) and i*SQUARE_LENTH <= mouse_pos[0] < SQUARE_LENTH*(i + 1) and board_code[j][i] != 0:
-                hold_piece = True
-                board_pieces_surfaces_position[j][i] = mouse_pos
+            if mouse_pressed and (j*SQUARE_LENTH <= mouse_state[1] <= SQUARE_LENTH*(j + 1)) and (i*SQUARE_LENTH <= mouse_state[0] <= SQUARE_LENTH*(i + 1)) and board_code[j][i] != 0 and piece_selected_code == None:
+                piece_selected_code = [j, i]
 
+def drop_piece():
+    global piece_selected_code, mouse_state
+    j = piece_selected_code[0]
+    i = piece_selected_code[1]
+    
+    board_pieces_surfaces_position[j][i][0] += SQUARE_LENTH/2
+    board_pieces_surfaces_position[j][i][1] += SQUARE_LENTH/2
+    for k in range(0, 8):
+        for n in range(0, 8):
+            if (n*SQUARE_LENTH <= board_pieces_surfaces_position[j][i][1] <= SQUARE_LENTH*(n + 1)) and (k*SQUARE_LENTH <= board_pieces_surfaces_position[j][i][0] <= SQUARE_LENTH*(k + 1)):
+                print(f'{board_pieces_surfaces_position[j][i]} /{j} e {i} / {n} e {k}')
+                board_code[k][n] = board_code[j][i] 
+                board_code[j][i] = 0
 
+                board_pieces_surfaces[k][n] = board_pieces_surfaces[j][i]
+                board_pieces_surfaces[j][i] = 0
+
+                board_pieces_surfaces_position[k][n] = [k*SQUARE_LENTH, n*SQUARE_LENTH]
+                board_pieces_surfaces_position[j][i] = [0, 0]
+
+                piece_selected_code = None
+                
+                
 
 piece()
 clock = pygame.time.Clock()
@@ -79,16 +102,20 @@ while True:
 
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_pressed = False
-            hold_piece = False
+            drop_piece()
             
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pressed = True
-            
-
-    
-    grab_piece(mouse_state)
+            grab_piece()
 
     board()
+    
+    if mouse_pressed:
+        if piece_selected_code != None:
+            j = piece_selected_code[0]
+            i = piece_selected_code[1]
+            board_pieces_surfaces_position[j][i] = [mouse_state[0]-SQUARE_LENTH/2, mouse_state[1]-SQUARE_LENTH/2]
+
 
     pygame.display.flip()
     pygame.display.update()
