98762351dda65bd16b391d4a4e019d92a39b3a2e
André Pinheiro <andreftmarcelo@gmail.com>
2022-07-02 10:24:57 +0100

    Add check_pieces function

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ board_code = [
 
 board_pieces_surfaces = [[0 for j in range(0, 8)] for i in range(0, 8)]
 board_pieces_surfaces_position = [[[0, 0] for j in range(0, 8)] for i in range(0, 8)]
+piece_selected_code = None
 
 def board():
     white = (243, 211, 160)
@@ -56,7 +57,6 @@ def piece():
             board_pieces_surfaces_position[j][i][0] = i*SQUARE_LENTH
             board_pieces_surfaces_position[j][i][1] = j*SQUARE_LENTH
 
-piece_selected_code = None
 
 def grab_piece():
     global piece_selected_code, mouse_state
@@ -67,27 +67,68 @@ def grab_piece():
 
 def drop_piece():
     global piece_selected_code, mouse_state
-    j = piece_selected_code[0]
-    i = piece_selected_code[1]
+    if piece_selected_code != None:
+        j = piece_selected_code[0]
+        i = piece_selected_code[1]
+        
+        board_pieces_surfaces_position[j][i][0] += SQUARE_LENTH/2
+        board_pieces_surfaces_position[j][i][1] += SQUARE_LENTH/2
+        for k in range(0, 8):
+            for n in range(0, 8):
+                if (n*SQUARE_LENTH <= mouse_state[1] <= SQUARE_LENTH*(n + 1)) and (k*SQUARE_LENTH <= mouse_state[0] <= SQUARE_LENTH*(k + 1)):
+                    if n != j or k != i:
+                        if check_move([j, i], [n, k]):
+                            print(f'{board_pieces_surfaces_position[j][i]} / {j} e {i} / {n} e {k}')
+                            board_code[n][k] = board_code[j][i] 
+                            board_code[j][i] = 0
+
+                            board_pieces_surfaces[n][k] = board_pieces_surfaces[j][i]
+                            board_pieces_surfaces[j][i] = 0
+
+                            board_pieces_surfaces_position[n][k] = [k*SQUARE_LENTH, n*SQUARE_LENTH]
+                            board_pieces_surfaces_position[j][i] = [0, 0]
+
+                            piece_selected_code = None
+                        else:
+                            board_pieces_surfaces_position[j][i] = [i*SQUARE_LENTH, j*SQUARE_LENTH]
+                            piece_selected_code = None
+
+                    else: 
+                        board_pieces_surfaces_position[j][i] = [i*SQUARE_LENTH, j*SQUARE_LENTH]
+                        piece_selected_code = None
+                
+
+def check_move(init, end):
+    init_square_code = board_code[init[0]][init[1]]
+    end_square_code = board_code[end[0]][end[1]]
+    if np.sign(init_square_code) != np.sign(end_square_code):
+        delta_x = end[1] - init[1]
+        delta_y = end[0] - init[0]
+
+        if abs(init_square_code) == 6:
+            return True
+        if abs(init_square_code) == 5:
+            if (abs(delta_x) == 2 and abs(delta_y) == 1) or (abs(delta_y) == 2 and abs(delta_x) == 1):
+                return True
+        if abs(init_square_code) == 4:
+            if abs(delta_x) <= 1 and abs(delta_y) <= 1:
+                return True
+        if abs(init_square_code) == 3:
+            if abs(delta_x) == abs(delta_y) or (abs(delta_x) == 0 or abs(delta_y) == 0):
+                return True
+        if abs(init_square_code) == 2:
+            if abs(delta_x) == abs(delta_y):
+                return True
+        if abs(init_square_code) == 1:
+            if abs(delta_x) == 0 or abs(delta_y) == 0:
+                return True
+
+        return False
+    else:
+        return False
+
     
-    board_pieces_surfaces_position[j][i][0] += SQUARE_LENTH/2
-    board_pieces_surfaces_position[j][i][1] += SQUARE_LENTH/2
-    for k in range(0, 8):
-        for n in range(0, 8):
-            if (n*SQUARE_LENTH <= board_pieces_surfaces_position[j][i][1] <= SQUARE_LENTH*(n + 1)) and (k*SQUARE_LENTH <= board_pieces_surfaces_position[j][i][0] <= SQUARE_LENTH*(k + 1)):
-                print(f'{board_pieces_surfaces_position[j][i]} /{j} e {i} / {n} e {k}')
-                board_code[k][n] = board_code[j][i] 
-                board_code[j][i] = 0
 
-                board_pieces_surfaces[k][n] = board_pieces_surfaces[j][i]
-                board_pieces_surfaces[j][i] = 0
-
-                board_pieces_surfaces_position[k][n] = [k*SQUARE_LENTH, n*SQUARE_LENTH]
-                board_pieces_surfaces_position[j][i] = [0, 0]
-
-                piece_selected_code = None
-                
-                
 
 piece()
 clock = pygame.time.Clock()
